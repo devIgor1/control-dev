e457d9d058e95ad4b681c5d531f2e2e161c814ab
Igor <igormraesmm@gmail.com>
2023-11-23 12:33:06 -0300

    feat: finish header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,24 @@
+import { FiUser, FiLogOut } from "react-icons/fi"
+import Link from "next/link"
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="w-full flex items-center px-2 py-4 bg-white h-20 shadow-lg">
+      <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+        <Link href="/">
+          <h1 className="font-bold text-2xl pl-1 hover:tracking-wide duration-300">
+            <span className="text-blue-500">Dev</span> Control
+          </h1>
+        </Link>
+        <div className="flex items-baseline gap-4">
+          <Link href="/dashboard">
+            <FiUser size={26} color="#4b5563" />
+          </Link>
+          <button>
+            <FiLogOut size={26} color="#4b5563" />
+          </button>
+        </div>
+      </div>
     </header>
   )
 }
