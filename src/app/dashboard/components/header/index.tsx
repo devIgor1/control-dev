4bc6d4db8962cebf8381d7fcb408e0780262a1ee
Igor <igormraesmm@gmail.com>
2023-11-26 12:33:11 -0300

    feat: add customer, using SSR for customer route

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -4,11 +4,17 @@ import Link from "next/link"
 export function DashboardHeader() {
   return (
     <Container>
-      <header className="w-full bg-gray-900 my-4 p-3 rounded flex gap-4 text-white">
-        <Link className="hover:text-zinc-400" href="/dashboard">
+      <header className="w-full bg-gray-900 my-4 p-3 rounded flex gap-4">
+        <Link
+          href="/dashboard"
+          className="text-white hover:font-bold duration-300"
+        >
           Tickets
         </Link>
-        <Link className="hover:text-zinc-400" href="/dashboard/customer">
+        <Link
+          href="/dashboard/customer"
+          className="text-white hover:font-bold duration-300"
+        >
           Customers
         </Link>
       </header>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import "./globals.css"
 import { Header } from "@/components/header"
 import { AuthProvider } from "@/providers/auth"
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["800", "400"] })
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const metadata: Metadata = {
   title: "Dev Control - Your management system ",
