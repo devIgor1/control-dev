2ff3873b3a62433ec3e69260b737ef2ea55d3148
Igor <igormraesmm@gmail.com>
2023-11-28 13:46:52 -0300

    feat: registering customers

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,14 +1,19 @@
-export function CardCustomer() {
+import { CustomerProps } from "@/utils/customer.type"
+
+export function CardCustomer({ customer }: { customer: CustomerProps }) {
   return (
     <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
       <h2>
-        <span className="font-bold">Name: </span>Mercado Silva
+        <span className="font-bold">Name: </span>
+        {customer.name}
       </h2>
       <p>
-        <span className="font-bold">Email: </span>teste@teste.com
+        <span className="font-bold">Email: </span>
+        {customer.email}
       </p>
       <p>
-        <span className="font-bold">Phone: </span>XX99999999
+        <span className="font-bold">Phone: </span>
+        {customer.phone}
       </p>
       <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
         Delete
