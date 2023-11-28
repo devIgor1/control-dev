f0d46fb8c7a59039ee98caca3e0309a81402a3cd
Igor <igormraesmm@gmail.com>
2023-11-28 14:34:11 -0300

    feat: DELETE route

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,42 @@ import { NextResponse } from "next/server"
 import { authOptions } from "@/lib/auth"
 import prisma from "@/lib/prisma"
 
+//Deleting customers
+export async function DELETE(req: Request) {
+  const session = await getServerSession(authOptions)
+
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+  }
+
+  const { searchParams } = new URL(req.url)
+
+  const userId = searchParams.get("id")
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: "Failure when trying to delete a client" },
+      { status: 400 }
+    )
+  }
+
+  try {
+    await prisma.customer.delete({
+      where: {
+        id: userId as string,
+      },
+    })
+
+    return NextResponse.json({ message: "Client deleted successfully!" })
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failure when trying to delete a client" },
+      { status: 400 }
+    )
+  }
+}
+
+//Registering customers
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions)
 
--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,6 +1,18 @@
+"use client"
+
+import { api } from "@/lib/api"
 import { CustomerProps } from "@/utils/customer.type"
 
 export function CardCustomer({ customer }: { customer: CustomerProps }) {
+  async function handleDeleteCustomer() {
+    try {
+      const response = await api.delete("/api/customer", {
+        params: {
+          id: customer.id,
+        },
+      })
+    } catch (error) {}
+  }
   return (
     <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
       <h2>
@@ -15,7 +27,10 @@ export function CardCustomer({ customer }: { customer: CustomerProps }) {
         <span className="font-bold">Phone: </span>
         {customer.phone}
       </p>
-      <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
+      <button
+        className="bg-red-500 px-4 rounded text-white mt-2 self-start"
+        onClick={handleDeleteCustomer}
+      >
         Delete
       </button>
     </article>
