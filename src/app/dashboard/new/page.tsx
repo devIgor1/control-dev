e11f82a6ff0c7a97c9447e0826fd66dd69ed67b5
Igor <igormraesmm@gmail.com>
2023-11-29 14:41:20 -0300

    feat: use server actions on the new tickets page

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -18,6 +18,13 @@ export default async function NewTicket() {
     },
   })
 
+  async function handleRegisterTicket(formData: FormData) {
+    "use server"
+    const title = formData.get("title")
+    const description = formData.get("description")
+    const customerId = formData.get("customer")
+  }
+
   return (
     <>
       {customers.length !== 0 ? (
@@ -32,22 +39,27 @@ export default async function NewTicket() {
               </Link>
               <h1 className="text-3xl font-bold">New Ticket</h1>
             </div>
-            <form className="flex flex-col mt-6">
+            <form className="flex flex-col mt-6" action={handleRegisterTicket}>
               <label className="mb-1 font-medium text-lg">Title</label>
               <input
                 className="w-full border-2 rounded-md px-2 mb-2 h-11"
                 type="text"
                 required
+                name="title"
               />
               <label className="mb-1 font-medium text-lg">Description</label>
               <textarea
                 className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-none pt-2"
                 required
+                name="description"
               ></textarea>
               <label className="mb-1 font-medium text-lg">
                 Select the customer
               </label>
-              <select className="w-full border-2 rounded-md px-2 mb-2 h-11  bg-white">
+              <select
+                className="w-full border-2 rounded-md px-2 mb-2 h-11  bg-white"
+                name="customer"
+              >
                 {customers.map((customer) => (
                   <option value={customer.id} key={customer.id}>
                     {customer.name}
