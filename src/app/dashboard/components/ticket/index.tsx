0c67539fb8e048aff572b80397ea760951696bf1
Igor <igormraesmm@gmail.com>
2023-12-01 11:24:37 -0300

    feat: add patch ticket function

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,5 +1,9 @@
+"use client"
+
+import { api } from "@/lib/api"
 import { CustomerProps } from "@/utils/customer.type"
-import { FiTrash2, FiFile } from "react-icons/fi"
+import { FiFile, FiCheckSquare } from "react-icons/fi"
+import { useRouter } from "next/navigation"
 
 interface TicketItemProps {
   ticket: TicketProps
@@ -7,6 +11,20 @@ interface TicketItemProps {
 }
 
 export function TicketItem({ customer, ticket }: TicketItemProps) {
+  const router = useRouter()
+
+  async function handleChangeStatus() {
+    try {
+      const response = await api.patch("/api/ticket", {
+        id: ticket.id,
+      })
+
+      router.refresh()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
@@ -20,8 +38,8 @@ export function TicketItem({ customer, ticket }: TicketItemProps) {
           </span>
         </td>
         <td className="text-left">
-          <button className="mr-2">
-            <FiTrash2 size={24} color="#EF4444" />
+          <button className="mr-2" onClick={handleChangeStatus}>
+            <FiCheckSquare size={24} color="#121212" />
           </button>
           <button>
             <FiFile size={24} color="3B82F6" />
