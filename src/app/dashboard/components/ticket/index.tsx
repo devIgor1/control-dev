bad0a014fc3dab41e8b74abe3bd2d6ece1e4bd32
Igor <igormraesmm@gmail.com>
2023-12-01 10:16:43 -0300

    feat: registering tickets feat: listing  tickets

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,13 +1,23 @@
+import { CustomerProps } from "@/utils/customer.type"
 import { FiTrash2, FiFile } from "react-icons/fi"
 
-export function TicketItem() {
+interface TicketItemProps {
+  ticket: TicketProps
+  customer: CustomerProps | null
+}
+
+export function TicketItem({ customer, ticket }: TicketItemProps) {
   return (
     <>
       <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
-        <td className="text-left pl-1">Igor Moraes</td>
-        <td className="text-left">06/02/1955</td>
+        <td className="text-left pl-1">{customer?.name}</td>
         <td className="text-left">
-          <span className="bg-green-500 px-2 py-1 rounded-lg">OPEN</span>
+          {ticket.created_at?.toLocaleDateString("pt-br")}
+        </td>
+        <td className="text-left">
+          <span className="bg-green-500 px-2 py-1 rounded-lg">
+            {ticket.status}
+          </span>
         </td>
         <td className="text-left">
           <button className="mr-2">
