dd59ec857d81892915c4b805859b699963893bb7
Igor <igormraesmm@gmail.com>
2023-12-02 12:33:31 -0300

    feat: add option to non-logged users send ticket

---
+++ src/app/open/components/FormTicket/index.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { Input } from "@/components/input"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const schema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+})
+
+type FormData = z.infer<typeof schema>
+
+export function FormTicket() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  })
+
+  return (
+    <form className="px-4 py-6 mt-16 rounded border-2 ">
+      <div className="mb-5">
+        <label className="mb-5 font-medium text-lg">Ticket name</label>
+        <Input
+          register={register}
+          type="text"
+          placeholder="Enter the ticket name"
+          name="name"
+          error={errors.name?.message}
+        />
+      </div>
+      <label className="mb-1 font-medium text-lg">Description</label>
+      <textarea
+        className="w-full border-2 rounded-md h-24 resize-none mb-2 px-2 py-2"
+        placeholder="Describe your problem"
+        id="description"
+        {...register("description")}
+      ></textarea>
+      {errors.description?.message && (
+        <p className="text-red-500 my-1">{errors.description.message}</p>
+      )}
+      <button
+        type="submit"
+        className="bg-blue-500 rounded-md w-full h-11 px-2 text-white font-bold"
+      >
+        Submit
+      </button>
+    </form>
+  )
+}
---
+++ src/app/open/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { Input } from "@/components/input"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { FiSearch, FiX } from "react-icons/fi"
+import { useState } from "react"
+import { FormTicket } from "./components/FormTicket"
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Enter the customer email address please")
+    .min(1, "Email is required"),
+})
+
+type FormData = z.infer<typeof schema>
+
+interface CustomerInfo {
+  id: string
+  name: string
+}
+
+export default function OpenTicket() {
+  const [customer, setCustomer] = useState<CustomerInfo | null>()
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  })
+
+  function handleClearCustomer() {
+    setCustomer(null)
+    setValue("email", "")
+  }
+
+  return (
+    <div className="w-full max-w-2xl mx-auto px-2">
+      <h1 className="text-3xl text-center mt-24 font-medium">Open a ticket</h1>
+      <main className="flex flex-col mt-4 mb-2">
+        {customer ? (
+          <div className="bg-slate-200 py-6 px-2 rounded border-2 flex items-center justify-between">
+            <p className="text-lg">
+              <strong>Selected customer: </strong>
+              <span className="text-blue-500 ml-2">{customer.name}</span>
+            </p>
+            <button
+              className="h-11 px-2 flex items-center justify-center rounded"
+              onClick={handleClearCustomer}
+            >
+              <FiX size={28} color={"#dc143c"} />
+            </button>
+          </div>
+        ) : (
+          <form>
+            <div className="relative">
+              <Input
+                name="email"
+                placeholder="Enter the customer email address please"
+                type="text"
+                error={errors.email?.message}
+                register={register}
+              />
+              <button className="w-full bg-blue-500 flex flex-row items-center justify-center gap-2 h-11 text-white font-medium rounded-b absolute inset-0 top-10 active:translate-y-2 duration-300">
+                Search Customer <FiSearch size={28} />
+              </button>
+            </div>
+          </form>
+        )}
+
+        {customer !== null && <FormTicket />}
+      </main>
+    </div>
+  )
+}
