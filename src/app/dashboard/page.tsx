84debb27c25a41b780ecce627bb9b4668e665485
Igor <igormraesmm@gmail.com>
2023-12-02 10:12:25 -0300

    feat: conditional rendering

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,39 +21,46 @@ export default async function Dashboard() {
     include: {
       customer: true,
     },
+    orderBy: {
+      created_at: "desc",
+    },
   })
 
   return (
     <Container>
       <main className="mt-9 mb-2">
-        <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold">Tickets</h1>
-          <Link
-            href="/dashboard/new"
-            className="bg-blue-500 px-4 py-1 rounded text-white"
-          >
-            Open a new ticket
-          </Link>
-        </div>
-        <table className="min-w-full my-2">
-          <thead>
-            <tr>
-              <th className="font-medium text-left pl-2">CUSTOMER</th>
-              <th className="font-medium text-left">REGISTRATION DATE</th>
-              <th className="font-medium text-left">STATUS</th>
-              <th className="font-medium text-left">#</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tickets.map((ticket) => (
-              <TicketItem
-                key={ticket.id}
-                customer={ticket.customer}
-                ticket={ticket}
-              />
-            ))}
-          </tbody>
-        </table>
+        {tickets.length > 0 && (
+          <>
+            <div className="flex items-center justify-between">
+              <h1 className="text-3xl font-bold">Tickets</h1>
+              <Link
+                href="/dashboard/new"
+                className="bg-blue-500 px-4 py-1 rounded text-white"
+              >
+                Open a new ticket
+              </Link>
+            </div>
+            <table className="min-w-full my-2">
+              <thead>
+                <tr>
+                  <th className="font-medium text-left pl-2">CUSTOMER</th>
+                  <th className="font-medium text-left">REGISTRATION DATE</th>
+                  <th className="font-medium text-left">STATUS</th>
+                  <th className="font-medium text-left">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tickets.map((ticket) => (
+                  <TicketItem
+                    key={ticket.id}
+                    customer={ticket.customer}
+                    ticket={ticket}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
 
         {tickets.length === 0 && (
           <h1 className="mt-10 text-2xl">
